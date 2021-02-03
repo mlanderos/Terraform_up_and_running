ee9c45e3fa7f045aa29ec5906102159df945690c
Michael Landeros <mlanderos@Michaels-Air.hsd1.il.comcast.net>
2021-02-03 17:16:25 -0600

    pull subnet IDs from aws_subnet_ids data source

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ data "aws_vpc" "default" {
 
 resource "aws_autoscaling_group" "example" {
     launch_configuration  = aws_launch_configuration.example.name
+    vpc_zone_identifier   = data.aws_subnet_ids.default.ids
 
     min_size = 2
     max_size = 10
